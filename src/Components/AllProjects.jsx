2f9578cc1c75d425e0fa2d7bcfb8ab07ff68e659
omarhamed2301 <omarhamed5500@gmail.com>
2024-12-01 16:43:08 +0200

    Changes

--- src/Components/AllProjects.jsx
+++ src/Components/AllProjects.jsx
@@ -7,6 +7,7 @@ import PizzaComp from "../Images/PizzaComp.png";
 import Ecommerce from "../Images/ecommerce.png";
 import Splitbill from "../Images/splitbill.png";
 import bankSystem from "../Images/bankSystem.png";
+import CRUDS from "../Images/CRUDS.png";
 
 export default function AllProjects() {
   return (
@@ -34,13 +35,6 @@ export default function AllProjects() {
               src={PizzaComp}
             />
             <PortfolioCard
-              title="Products E-commerce using State Management & Fake API"
-              href="https://ecommerce-lilac-nu.vercel.app/"
-              description="A responsive e-commerce website showcasing a range of products, where users can view details and add items to their cart. The site leverages state management and integrates with an API to dynamically display products, ensuring smooth navigation and a seamless cart experience for efficient and convenient shopping."
-              github="https://github.com/omarhamed2301/ecommerce"
-              src={Ecommerce}
-            />
-            <PortfolioCard
               title="FairShare: Simplified Bill Splitting for Friends"
               href="https://splitbill-tau.vercel.app/"
               description="FairShare offers a streamlined way to manage shared expenses with friends. This responsive React app accurately tracks balances, identifies who owes whom, and checks if everything evens out. Built with a focus on responsive design and smooth state management."
@@ -53,6 +47,20 @@ export default function AllProjects() {
               description="FlexiBank is a simplified banking system built with React and Redux for state management. This project offers essential banking functionalities, including account creation, balance management, loan requests, and repayments, all wrapped in a responsive and user-friendly interface. The design incorporates modern CSS techniques to enhance usability and aesthetics."
               github="https://github.com/omarhamed2301/bank-system"
               src={bankSystem}
+            />
+            <PortfolioCard
+              title="Products E-commerce using State Management & Fake API"
+              href="https://ecommerce-lilac-nu.vercel.app/"
+              description="A responsive e-commerce website showcasing a range of products, where users can view details and add items to their cart. The site leverages state management and integrates with an API to dynamically display products, ensuring smooth navigation and a seamless cart experience for efficient and convenient shopping."
+              github="https://github.com/omarhamed2301/ecommerce"
+              src={Ecommerce}
+            />
+            <PortfolioCard
+              title="CRUD React Application using Content API & State Management"
+              href="https://cruds-sage.vercel.app/"
+              description="A dynamic React application showcasing CRUD functionality: Create, Read, Update, and Delete. Built with the Context API for efficient state management, the app delivers a seamless user experience with real-time updates and intuitive operations. Perfectly crafted to demonstrate practical skills in managing and manipulating data in a React environment."
+              github="https://github.com/omarhamed2301/CRUDS"
+              src={CRUDS}
             />
             
           </div>
